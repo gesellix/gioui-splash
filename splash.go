27d8a52f222d6cdabedbb615977b1f7deae9d6c4
Chris Waldon <christopher.waldon.dev@gmail.com>
2023-06-23 11:04:26 -0400

    splash,cmd: update to handle units consistently
    This commit fixes a unit mixup that caused confusion. Previously the main program
    was responsible for providing a rectangle, and this rectangle was used as the bounding
    box of the progress bar. However, this rectangle was in screen pixels, and thus wasn't
    being scaled using gtx.Dp. It would be the same number of physical pixels no matter the
    system scaling factor.
    
    This commit restructures the splash widget to accept the height and inset of the progress
    bar in Dp, and the splash widget itself handles converting these to screen pixels during
    layout. Delaying this unit conversion until the point of layout ensures that even if the
    window changes scaling factors, the UI will respond appropriately on the next frame.
    
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- splash.go
+++ splash.go
@@ -1,38 +1,57 @@
 package splash
 
 import (
+	"image"
+	"image/color"
+	"log"
+
 	"gioui.org/layout"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
+	"gioui.org/unit"
 	"gioui.org/widget"
-	"image"
-	"image/color"
 )
 
 type Splash struct {
-	img          image.Image
-	progressRect image.Rectangle
-	progressCol  color.NRGBA
-	progress     float64
+	img            image.Image
+	progressHeight unit.Dp
+	progressCol    color.NRGBA
+	progress       float64
+	progressInset  layout.Inset
 }
 
-func NewSplash(img image.Image, progressRect image.Rectangle, progressCol color.NRGBA) *Splash {
+func NewSplash(img image.Image, progressHeight unit.Dp, progressInset unit.Dp, progressCol color.NRGBA) *Splash {
 	return &Splash{
 		img,
-		progressRect,
+		progressHeight,
 		progressCol,
 		0,
+		layout.UniformInset(progressInset),
 	}
 }
 
 func (s *Splash) SetProgress(progress float64) {
 	s.progress = progress
+	if s.progress > 1 {
+		s.progress = 1
+	}
+	if s.progress < 0 {
+		s.progress = 0
+	}
 }
 
 func (s *Splash) Layout(gtx layout.Context) layout.Dimensions {
-	dimensions := s.drawLogo(gtx)
-	s.drawProgress(gtx, s.progress)
-	return dimensions
+	// Lay out the image and overlay the progress bar on top of it, aligning
+	// to the south so that the progress bar is at the bottom.
+	return layout.Stack{
+		Alignment: layout.S,
+	}.Layout(gtx,
+		layout.Stacked(s.drawLogo),
+		layout.Expanded(func(gtx layout.Context) layout.Dimensions {
+			// Lay out the progress bar within the inset.
+			return s.progressInset.Layout(gtx, s.drawProgress)
+		}),
+	)
 }
 
 func (s *Splash) drawLogo(gtx layout.Context) layout.Dimensions {
@@ -42,17 +61,30 @@ func (s *Splash) drawLogo(gtx layout.Context) layout.Dimensions {
 	}.Layout(gtx)
 }
 
-func (s *Splash) drawProgress(gtx layout.Context, progress float64) {
-	spread := s.progressRect.Max.X - s.progressRect.Min.X
+func (s *Splash) drawProgress(gtx layout.Context) layout.Dimensions {
+	// Our gtx.Constraints.Min.X provide the width that a full progress bar
+	// must occupy.
+	spread := gtx.Constraints.Min.X
 	rectangle := image.Rectangle{
-		Min: s.progressRect.Min,
 		Max: image.Pt(
-			s.progressRect.Min.X+int(progress*float64(spread)),
-			s.progressRect.Max.Y,
+			// Since spread is already a unit of screen pixels, no need to pass through gtx.Dp
+			// to convert to them.
+			int(float64(spread)*s.progress),
+			// Convert our progress bar height to screen pixels.
+			gtx.Dp(s.progressHeight),
 		),
 	}
+	log.Printf("spread: %v progress: %v minX: %v", spread, s.progress, gtx.Constraints.Min.X)
 	paint.FillShape(
 		gtx.Ops,
 		s.progressCol,
 		clip.Rect(rectangle).Op())
+	// Return the logical dimensions occupied by the progress bar, which is the width of a full
+	// bar.
+	return layout.Dimensions{
+		Size: image.Point{
+			X: gtx.Constraints.Min.X,
+			Y: gtx.Dp(s.progressHeight),
+		},
+	}
 }
